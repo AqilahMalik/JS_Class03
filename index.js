f40faed44485e51f381a4f62809af6448cc5f989
AqilahMalik <aqilahsimaa98@gmail.com>
2021-08-24 14:54:00 +0800

    Finished working with API

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 //  Working with APIs
 //  We need to answer the following questions
 //  - What is the endpoint (URL)?
@@ -7,3 +5,17 @@ console.log("Hello World");
 //    - What are the required parameters?
 //    - What optional parameters do we want to add?
 //  - What HTTP method are we using?
+
+let url = `https://restcountries.eu/rest/v2/name/Australia`;
+
+function waitForData(res) {
+  return res.json();
+}
+
+function handleData(data) {
+  //debugger; //stop running js, and give access to that data
+  let pop = data[0].population;
+  console.log(`The population of Australia is ${pop}`);
+}
+
+fetch(url, { method: "GET" }).then(waitForData).then(handleData);
